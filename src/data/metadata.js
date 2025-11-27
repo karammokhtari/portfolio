c0bb9ed78381577e529b8d8b4045540d32dde6f7
karammokhtari <karam.mokhtari@gmail.com>
2025-11-27 04:43:56 +0100

    Update metadata.js

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -10,7 +10,7 @@ export const profileMetadata = {
     label: 'LinkedIn',
   },
   description: [
-    'Art Director and Music Rights Director with 10+ years of experience leading visual campaigns and managing master and publishing rights. Leads campaign visuals that align with release strategy while ensuring accurate metadata registration and traceability for proper royalty collection.',
+    'TEST, Art Director and Music Rights Director with 10+ years of experience leading visual campaigns and managing master and publishing rights. Leads campaign visuals that align with release strategy while ensuring accurate metadata registration and traceability for proper royalty collection.',
     '3× RIAA Gold Certifications, Billboard placements',
   ],
 }
